65bf13746040351144a403b81dde3985763a3687
svetlana <R_S_U@outlook.com>
2023-01-17 21:45:35 +1000

    add Example004_Div

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите ваше имя");
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "Мария") 
+{
+    Console.WriteLine("Ура! Это же Мария!");
+}
+else 
+{
+    Console.WriteLine("Привет, ", userName);
+}
